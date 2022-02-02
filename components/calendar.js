52676e3f78b6122ca337966a2c0f43e1753cf4ef
mhyneman8 <mallory.hyneman@gmail.com>
2022-02-02 17:57:39 -0600

    get rid of top bar markers

--- components/calendar.js
+++ components/calendar.js
@@ -5,8 +5,9 @@ import * as Calendar from 'expo-calendar';
 import { StatusBar } from 'expo-status-bar';
 import { TextInput } from 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
-import VideoChat from './video-chat';
-import Meeting from './meeting';
+
+// import VideoChat from './video-chat';
+// import Meeting from './meeting';
 
 
 // export default class CalendarScreen extends React.Component {
@@ -30,47 +31,30 @@ import Meeting from './meeting';
     //         ? selectedStartDate.format('MM-DD-YYYY').toString() 
     //         : '';
 
-async function getDefaultCalendarSource() {
-    const calendars = await Calendar.getCalendarsAsync(
-        Calendar.EntityTypes.EVENT
-    );
-    const defaultCalendars = calendars.filter(
-        (each) => each.source.name === 'Default'
-    );
-    return defaultCalendars.length
-        ? defaultCalendars[0].source
-        : calendars[0].source;
-}
-
-async function createCalendar() {
-    const defaultCalendarSource =
-        Platform.OS === 'ios'
-            ? await getDefaultCalendarSource()
-            : { isLocalAccount: true, name: 'Expo Calendar' };
-    const newCalendarID = await Calendar.createCalendarAsync({
-        title: 'Expo Calendar',
-        color: 'blue',
-        entityType: Calendar.EntityTypes.EVENT,
-        sourceId: defaultCalendarSource,
-        name: 'internalCalendarName',
-        ownerAccount: 'personal',
-        accessLevel: Calendar.CalendarAccessLevel.OWNER,
-    });
-}
-
 const Stack = createStackNavigator();
 
 export default function CalendarScreen({ navigation }) {
     const [selectedStartDate, setSelectedStartDate] = useState(null);
+    // const [granted, setGranted] = useState(false);
+    const [eventIdInCalendar, setEventIdInCalendar] = useState('');
+
     const [friendNameText, setFriendNameText] = useState('');
     const startDate = selectedStartDate
         ? selectedStartDate.format('MM-DD-YY').toString()
         : '';
 
+    const openCalendarRequest = async () => {
+        const {status} = await Permissions.askAsync(Permissions.CALENDAR)
+        if (status === 'granted') {
+            setGranted(true)
+        }
+    }
+
     useEffect(() => {
         (async () => {
             const { status } = await Calendar.requestCalendarPermissionsAsync();
-            if ( status === 'granted' ) {
+            
+            if (status === 'granted') {
                 const calendars = await Calendar.getCalendarsAsync(
                     Calendar.EntityTypes.EVENT
                 );
@@ -78,44 +62,75 @@ export default function CalendarScreen({ navigation }) {
                 // console.log({ calendars });
             }
         })();
-    })
+    }, []);
+
+    async function getDefaultCalendarSource() {
+        const calendars = await Calendar.getCalendarsAsync(
+            Calendar.EntityTypes.EVENT
+        );
+        const defaultCalendars = calendars.filter(
+            (each) => each.source.name === 'Default'
+        );
+        return defaultCalendars.length
+            ? defaultCalendars[0].source
+            : calendars[0].source;
+    }
+    
+    async function createCalendar() {
+        const defaultCalendarSource =
+            Platform.OS === 'ios'
+                ? await getDefaultCalendarSource()
+                : { isLocalAccount: true, name: 'Expo Calendar' };
+        const newCalendarID = await Calendar.createCalendarAsync({
+            title: 'Expo Calendar',
+            color: 'blue',
+            entityType: Calendar.EntityTypes.EVENT,
+            sourceId: defaultCalendarSource.id,
+            source: defaultCalendarSource,
+            name: 'internalCalendarName',
+            ownerAccount: 'personal',
+            accessLevel: Calendar.CalendarAccessLevel.OWNER,
+        });
+    }
 
     const addNewEvent = async () => {
         try {
-            const calendarId = await createCalendar();
+            const calendarId = await createCalendar().sourceId;
 
             const res = await Calendar.createEventAsync(calendarId, {
-                endDate: getAppointmentDate(startDate),
-                startDate: getAppointmentDate(startDate),
+                endDate: (startDate),
+                startDate: (startDate),
                 title: 'Happy Birthday buddy ' + friendNameText,
             });
+            Calendar.openEventInCalendar(res)
             Alert.alert('Event Created!');
         } catch (e) {
             console.log(e);
         }
     }
 
+    const getEvents = async () => {
+        const calendarId = await createCalendar();
+        const events = await Calendar.getEventsAsync(calendarId, startDate)
+        console.log(events);
+    }
+
     return (
       <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
         <StatusBar style="auto"/>
 
+        <CalendarPicker 
+            onDateChange={setSelectedStartDate} 
+            onChange={getEvents}
+        />
+        
+        <Text style={styles.dateText} >Selected Date: {startDate}</Text>
         <TextInput
             onChangeText={setFriendNameText}
             value={friendNameText}
             placeholder="Enter the name of your friend"
             style={styles.input}
         />
-
-        <Text>menu</Text>
-        <Text>Title</Text>
-        <Text>Notifications</Text>
-
-        <CalendarPicker 
-            onDateChange={setSelectedStartDate} 
-        />
-        
-        <Text style={styles.dateText} >Selected Date: {startDate}</Text>
-        
         <Button 
             title={"Add to Calendar"}
             onPress={addNewEvent}
@@ -124,7 +139,7 @@ export default function CalendarScreen({ navigation }) {
         <Text>Show tasks at bottom when clicked on day</Text>
         <Text>Click meeting event to see details</Text>
 
-        <Stack.Navigator>
+        {/* <Stack.Navigator>
             <Stack.Screen
                 name="VideoChat"
                 component={VideoChat}
@@ -133,17 +148,17 @@ export default function CalendarScreen({ navigation }) {
                 name="Meeting"
                 component={Meeting}
             />
-        </Stack.Navigator>
+        </Stack.Navigator> */}
 
-        <Button 
+        {/* <Button 
             title="VideoChat"
             onPress={() => navigation.navigate('VideoChat')}
-        />
+        /> */}
 
-        <Button
+        {/* <Button
           title="Meeting"
           onPress={() => navigation.navigate('Meeting')}
-        /> 
+        />  */}
 
       </View>
     )
