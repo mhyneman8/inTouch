50da6638c217dcb16508eb062ab8d4a165086240
mhyneman8 <mallory.hyneman@gmail.com>
2022-02-02 11:19:28 -0600

    fix nav icons

--- App.js
+++ App.js
@@ -5,8 +5,9 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { useTheme } from '@react-navigation/native';
 // import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 // screens
 import Fundraising from './components/fundraising';
@@ -18,7 +19,8 @@ import VideoChat from './components/video-chat';
 // screen name
 const homeScreen = "Connect";
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
 // const Stack = createStackNavigator();
 
 function App() {
@@ -27,53 +29,37 @@ function App() {
       <NavigationContainer>
         <Tab.Navigator
           initialRouteName={homeScreen}
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              let rn = route.name;
-
-              if(rn = homeScreen) {
-                iconName = focused ? 'home' : 'home-outline';
-              } else if (rn === Fundraising) {
-                iconName = focused ? 'list' : 'list-outline';
-              } else if (rn === CalendarScreen) {
-                iconName = focused ? 'settings' : 'settings-outline';
-              } 
-              
-              return <Ionicons name={iconName} size={size} color={color} />;
-              // else if (rn === Meeting) {
-              //   iconName = focused ? '' : '-outline'
-              // } else if (rn === VideoChat) {
-              //   iconName = focused ? '' : '-outline'
-              // }
-            },
-          })}
-          screenOptions={
-            {
-              "tabBarActiveTintColor": "tomato",
-              "tabBarInactiveTintColor": "grey",
-              "tabBarLabelStyle": { 
-                paddingBottom: 10, 
-                fontSize: 10
-              },
-              "tabBarStyle": [
-                {
-                  "display": "flex",
-                  "padding": 10, 
-                  "height": 70
-                },
-                null
-            ]
-            }
-          }
+          activeColor="#f0edf6"
+          inactiveColor="#3e2465"
+          barStyle={{ backgroundColor: '#694fad' }}
           >
           <Tab.Screen
             name="Fundraising"
             component={Fundraising}
+            options={{
+              tabBarLabel: 'Fundraising',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons
+                  name="bell"
+                  color={color}
+                  size={26}
+                  />
+              ),
+            }}
           />
           <Tab.Screen
             name="Connect"
             component={Connect}
+            options={{
+              tabBarLabel: 'Connect',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons 
+                  name="group" 
+                  color={color} 
+                  size={26} 
+                />
+              )
+            }}
           />
           {/* <Tab.Screen
             name="add"
@@ -81,12 +67,22 @@ function App() {
           <Tab.Screen
             name="Calendar"
             component={CalendarScreen}
+            options={{
+              tabBarLabel: 'Calendar',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons
+                  name="calendar"
+                  color={color}
+                  size={26}
+                />
+              )
+            }}
           />
-          <Tab.Screen
+          {/* <Tab.Screen
             name="Meeting"
             component={Meeting}
           />
-          {/* <Tab.Screen
+          <Tab.Screen
             name="VideoChat"
             component={VideoChat}
           /> */}
