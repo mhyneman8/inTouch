4df03c3b575ca4da7cd5ef49c37ba4624f2ec526
mhyneman8 <mallory.hyneman@gmail.com>
2022-02-02 20:00:08 -0600

    add new project button and functionality

--- components/fundraising.js
+++ components/fundraising.js
@@ -1,144 +1,143 @@
 import { uniqueId } from 'lodash';
 import React, { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
-// import { TextInput } from 'react-native-gesture-handler';
-import projects from './project-data';
-
-// export default class Fundraising extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       projectArray: [],
-        // {
-        //   name: "spider",
-        //   description: ""
-        // }
-      // ],
-  //     newProjectName: '',
-  //   }
-  // }
-  
-  // projectList =() => {
-  //     return projects.map((project) => {
-  //       return (
-  //         <View key={Element.key} style={{margin: 10}}>
-  //           <Text>{project.name}</Text>
-  //           <Text>{project.description}</Text>
-  //         </View>
-  //       );
-  //     });
-  //   };
+// import projects from './project-data';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
   export default function Fundraising() {
     const [projectName, setProjectName] = useState();
-    const [projectArray, setProjectArray] = useState([
-      {title: "title1", description: "Des1"},
-      {title: "title2", description: "Des2"},
-      {title: "title3", description: "Des3"},
-    ]);
+    const [projectGoal, setProjectGoal] = useState();
+    const [projectDescription, setProjectDescription] = useState();
+    const [shouldShow, setShouldShow] = useState(false);
+    const [showEdit, setShowEdit] = useState(false);
 
-    // const pName = projectArray.map(project => project.title)
+    const [projectArray, setProjectArray] = useState([
+      {title: "title1", description: "Des1", goal: 200},
+      {title: "title2", description: "Des2", goal: 400},
+      {title: "title3", description: "Des3", goal: 3030},
+    ]);
 
     const list = () => {
       return projectArray.map((project) => {
         return (
           <View key={project.title} style={{margin: 10}}>
+            {/* <MaterialCommunityIcons
+              name="dots"
+              color="purple"
+              size={20}
+            /> */}
+
+            <Button
+              title="..."
+              onPress={() => setShowEdit(!showEdit)}
+            />
+
             <Text>{project.title}</Text>
             <Text>{project.description}</Text>
+            <Text>{project.goal}</Text>
+            
+            { showEdit ? (
+              <View>
+            <MaterialCommunityIcons
+              name="delete"
+              color="purple"
+              size={36}
+            />
+            <MaterialCommunityIcons
+              name="pencil"
+              color="purple"
+              size={36}
+            />
+            </View>
+            ) : null }
           </View>
         );
       })
     }
- 
-  // console.log(projectName + " name")
-  // console.log(projectArray + " array")
-  //   const addProject = () => {
-  //     console.log(projectName)
 
-  //     let title = projectName;
-
-  //     AsyncStorage.getItem
-
-
-  //     setProjectArray = [ ...projectArray, projectName] 
-  //     // this.setState({ projectArray: [ projectName ] });
-  //     console.log(projectArray)
-  // }
-
-  // const newName = () => {
-  //   setProjectName
-  //   console.log(projectName)
-  // }
-
-  // render() {
-    // const project = projects;
-
-    const addNewProject = async () => {
-      setProjectArray({...projectArray, projectName})
+    const startNewProject = () => {
+      setProjectArray((projectArray) => 
+        [...projectArray, {title: projectName, description: projectDescription, goal: projectGoal}]
+      ),
+      setShouldShow(!shouldShow)
     }
 
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 
-        <Text>{list()}</Text>
+        <Text style={{ flex: 2, marginTop: 25 }} >
+          {list()}</Text>
         
-        <TextInput
-          style={styles.input}
-          value={projectName}
-          placeholder={"Enter new project name"}
-          onChangeText={setProjectName}
-          onSubmitEditing={() => setProjectArray((projectArray) => 
-              [...projectArray, {title: projectName, description: 'testing'}]
-            )}
-          />
+        {shouldShow ? (
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                value={projectName}
+                placeholder={"Enter new project name"}
+                onChangeText={setProjectName}
+              />
 
-          
+            <TextInput
+              style={styles.input}
+              value={projectGoal}
+              onChangeText={setProjectGoal}
+              placeholder={"Enter Goal Amount"}
+              name="goal"
+              keyboardType='numeric'
+            /> 
+            
+            <TextInput
+              style={styles.input}
+              value={projectDescription}
+              onChangeText={setProjectDescription}
+              placeholder={"Describe Project..."}
+              name="description"
+              multiline
+            /> 
 
-          {/* <TextInput
-          name="goal"
-            keyboardType='numeric'
-          /> */}
-          {/* <TextInput
-            name="description"
-            multiline
-          /> */}
+            <Button 
+              title="Submit"
+              style={{ width: 50, height: 50 }}
+              onPress={startNewProject}
+            />
 
-        {/* <Text>{this.state.projectList[0].name}</Text> */}
-        <View>
-           
-          {/* { (projectArray === null) 
-            ? <Text>Null</Text>
-            : <Text>{projectArray}</Text>
-          } */}
-          
-          {/* {this.state.projectList.map((item, index) => {
-            return (
-              <View key={item}>
-                <Text>{item}</Text>
-              </View>
-            )
-          })} */}
-        </View>
+          </View>
+        ) : null }
 
-          
+        
 
         {/* <Text>Project Pics and progress bar</Text>
         <Text>Click on project to see updates and description of fundraising project</Text>
+         */}
 
-        <Text>Dashboard</Text>
-        <Button title="add">+</Button> */}
-        
+        <View style={{ backgroundColor: "grey", borderRadius: 50, width: 60, height: 60}}>
+          <Button
+            title="New Project"
+            onPress={() => setShouldShow(!shouldShow)}
+          />
+          <MaterialCommunityIcons
+            name="plus"
+            color="purple"
+            size={36}
+          />  
+        </View>
       </View>
     )
-  // }
 }
 
 const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 2,
+    width: '100%',
+  },
   input: {
       height: 40,
+      width: '80%',
       margin: 12,
       borderWidth: 1,
+      justifyContent: 'center', 
+      alignItems: 'center'
   },
   dateText: {
       margin: 16,
