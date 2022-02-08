88ac802c027e893a35851c17c16208d1861622cc
mhyneman8 <mallory.hyneman@gmail.com>
2022-02-08 16:00:11 -0600

    meeting styling

--- components/meeting.js
+++ components/meeting.js
@@ -2,30 +2,72 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-export default class Meeting extends React.Component {
-  render() {
+export default function Meeting({ navigation }) {
     return (
       <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Back arrow</Text>
-        <Text>Notifications</Text>
-        
-        <Text>Calendar meeting info</Text>
-        <Text>Notes and edit option</Text>
-        <Text>Share documents</Text>
+        <View style={styles.container}>
+          <Text style={styles.time}>
+              7:00 pm - 7:30 pm
+          </Text>
+          <Text style={styles.text}>
+            {/* {eventEndTime}
+            {eventInvitees}
+            {eventNotes} */}
+            Video Call - Joanna
+          </Text>
+          
+        </View>
+        <View style={styles.bottomSection}>
+          <Text style={styles.subHeading}>
+              2 Participants
+          </Text>
+          <Text style={styles.subHeading}>
+              Notes
+          </Text>
+            <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+              <Text style={styles.subtext}>
+              Topics to be discussed
+              </Text>
+              <Text style={styles.subtext}>
+                - Current funding status
+              </Text>
+              <Text style={styles.subtext}>
+                - Next Event
+              </Text>
+              <Text style={styles.subtext}>
+                - New strategy
+              </Text>
+            </View>
+         
+          <Text sytles={styles.subHeading}>
+            Share documents
+          </Text>
+          
+          <View>
+            <TouchableOpacity 
+                style={styles.button}
+                onPress={() => navigation.navigate('VideoChat')}
+            >
+                <Text style={styles.buttonText}>
+                  Join the meeting
+                </Text>
+            </TouchableOpacity>
+          </View>
 
         {/* button to join meeting */}
-        <TouchableOpacity 
+        {/* <TouchableOpacity 
           style={styles.button}
           onPress={() => navigation.navigate('VideoChat')}
         >
-          <Text style={styles.buttonText}>Video Chat</Text>
-        </TouchableOpacity>
+          <Text style={styles.buttonText}>
+            Video Chat
+          </Text>
+        </TouchableOpacity> */}
         {/* button for call settings */}
-        
+        </View>
       </View>
     )
   }
-}
 
 const styles = StyleSheet.create({
   button: {
@@ -34,10 +76,56 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
+    position: 'absolute',
+    width: '90%',
+    height: 47,
+    // bottom: 0,
+    // flex: 2,
   }, 
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    width: '100%',
+  
+  },
   buttonText: {
     color: 'white',
     fontWeight: '700',
     fontSize: 16,
   },
+  subHeading: {
+    fontSize: 20,
+    margin: 20,
+    marginLeft: 50,
+    color: '#402F6B',
+    borderTopColor: 'grey'
+  },
+  text: {
+    fontSize: 20,
+    color: '#402F6B',
+    textAlign: 'center',
+    marginTop: 50,
+    justifyContent: 'center'
+  },
+  subtext: {
+    // display: 'block'
+    // marginLeft: 30
+    // flex: 1,
+    width: '10%',
+    flexDirection: 'row'
+  },
+  time: {
+    color: '#402f6b',
+    fontSize: 12,
+    left: 0,
+    top: 0,
+    // position: 'absolute',
+    marginTop: 20,
+    marginLeft: 40
+  },
+  bottomSection: {
+    backgroundColor: '#f5f3f8',
+    flex: 2,
+    width: '100%'
+  }
 })
--- components/video-chat.js
+++ components/video-chat.js
@@ -5,10 +5,30 @@ export default class VideoChat extends React.Component {
   render() {
     return (
       <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>menu</Text>
-        <Text>Title</Text>
-        <Text>Notifications</Text>
         
+        <View style={styles.container}>
+                <Text style={styles.text}>
+                    7:00 pm - 7:30 pm
+                    {eventStartTime}
+                    {/* {eventEndTime}
+                    {eventInvitees}
+                    {eventNotes} */}
+                    Video Call - Joanna
+                </Text>
+                <Text style={styles.subtext}>
+                    
+                </Text>
+                <Text style={styles.subtext}>
+                    7:00 pm - 7:30 pm
+                </Text>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={() => navigation.navigate('Meeting')}
+                >
+                    <Text style={styles.buttonText}>Meeting Information</Text>
+                </TouchableOpacity>
+            </View>
+
         <Text>Boxes to show video screens of participants</Text>
         
         <Text>menu to switch camera, mute camera, end call, mute mic, chat</Text>
